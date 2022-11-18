1d1ae819cb8d8ab1a7d606d4c666f34070640af2
PhoebeLi65893 <91997360+PhoebeLi65893@users.noreply.github.com>
2022-11-18 11:54:18 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-const Alexa = require(`ask-sdk-core`);
+//const Alexa = require(`ask-sdk-core`);
 const skillBuilder = Alexa.SkillBuilders.custom();
-const skillEventHandler = require('./src/handlers/skillEventHandler');
+//const skillEventHandler = require('./src/handlers/skillEventHandler');
 
 exports.handler = skillBuilder
   .addRequestHandlers(
-    skillEventHandler
+    //skillEventHandler
   )
   .lambda();
+
+if (request.directive.header.namespace === 'Alexa.Authorization' && request.directive.header.name === 'AcceptGrant') {
+    console.log("DEBUG:", "Authorization request",  JSON.stringify(request));
+    handleAcceptGrant(request, context);
+}
+
+function handleAcceptGrant(request, context) {
+    var response = {
+        event: {
+            header: {
+                "namespace": "Alexa.Authorization",
+                "name": "AcceptGrant.Response",
+                "messageId": request.directive.header.messageId,
+                "payloadVersion": "3"
+            },
+            payload: {}
+        }
+    };
+    console.log("DEBUG", "Alexa.Authorization ", JSON.stringify(response));
+    //context.succeed(response);
+    return response;
+}
